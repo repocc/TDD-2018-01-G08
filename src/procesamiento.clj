3eb12958968760e4a7ebba022a9d6f3b5277e3e6
mevk <masrianv@gmail.com>
2018-04-08 20:00:03 -0300

    Se devuelve false ante una expresion que consta de una palabra y que no sea true

--- src/procesamiento.clj
+++ src/procesamiento.clj
@@ -76,7 +76,7 @@
   (if (not (seq? funcion-con-argumentos))
     (if (= funcion-con-argumentos true)
       true
-      ERROR
+      false
     )
     (let [ 
           funcion     (obtener-nombre-funcion funcion-con-argumentos)
